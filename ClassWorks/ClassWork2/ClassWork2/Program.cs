74a26fd7d89a7904fcbb19bb9fef606fb8ee8695
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-16 20:36:17 +0400

    .

--- ClassWorks/ClassWork2/ClassWork2/Program.cs
+++ ClassWorks/ClassWork2/ClassWork2/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("The number is zero.");
+            }
+            else
+            {
+                Console.WriteLine("The number is negative.");
+            }
         }
     }
 }
+
