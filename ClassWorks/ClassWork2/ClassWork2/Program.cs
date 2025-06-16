559d60fbb5847ba6df66568227cedf704ecd9a02
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-16 20:49:04 +0400

    ClassWork2

--- ClassWorks/ClassWork2/ClassWork2/Program.cs
+++ ClassWorks/ClassWork2/ClassWork2/Program.cs
@@ -4,20 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            //დავალება 1: შეამოწმეთ, არის თუ არა რიცხვი დადებითი, ნულოვანი თუ უარყოფითი
+            //Console.WriteLine("Enter your number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
 
-            if (number > 0)
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("The number is positive.");
+            //}
+            //else if (number == 0)
+            //{
+            //    Console.WriteLine("The number is zero.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is negative.");
+            //}
+
+
+
+            //დავალება 2
+            Console.WriteLine("Enter your char: ");
+            char character = Convert.ToChar(Console.ReadLine());
+
+            if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ||
+               character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U')
             {
-                Console.WriteLine("The number is positive.");
-            }
-            else if (number == 0)
-            {
-                Console.WriteLine("The number is zero.");
+                Console.WriteLine("The character is a vowel.");
             }
             else
             {
-                Console.WriteLine("The number is negative.");
+                Console.WriteLine("The character is a consonant.");
+
             }
         }
     }
