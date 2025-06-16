8260c828415a2944e8948852f00f1f826a37f8fd
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-16 11:23:59 +0400

    .

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -10,9 +10,49 @@
             //არითმეტიკული           + - * /    += -= *= /=    ++ --
             //დამხმარე ოპერატორები    && - და     || - ან                               & |
 
-            int x = 10;
-            Console.WriteLine(x); // მინიჭების ოპერატორი
 
+            // მინიჭების ოპერატორი
+            int x = 10;
+            Console.WriteLine(x);
+
+            // შედარების ოპერატორები
+            Console.WriteLine(5 == 5); //true
+            Console.WriteLine(10>5); //true
+            Console.WriteLine(10>10); //false
+            Console.WriteLine(8<12); //true
+            Console.WriteLine(10>=7); //true
+            Console.WriteLine(10<=10); //true
+            Console.WriteLine(10<=8); //false
+            Console.WriteLine(3!=5); //true
+
+            //არითმეტიკული
+            Console.WriteLine(11+14); //25
+            Console.WriteLine(15-12); //3
+            Console.WriteLine(35*20); //700
+            Console.WriteLine(42 / 6); //7
+
+            int y = 0;
+            y += 2;
+            Console.WriteLine(y); //2
+            y -= 3;
+            Console.WriteLine(y); //-1
+            y *= -5;
+            Console.WriteLine(y); //5
+            y /= 5;
+            Console.WriteLine(y); //1
+
+            y = y++;
+            Console.WriteLine(y); //1
+            y = ++y;
+            Console.WriteLine(y); //2
+            y = y--;
+            Console.WriteLine(y); //2
+            y = --y;
+            Console.WriteLine(y); //1
+
+            //დამხმარე ოპერატორები
+            Console.WriteLine(12>10 && 2>1); // true
+            Console.WriteLine(12<10 || 2>1); // true
 
 
 
