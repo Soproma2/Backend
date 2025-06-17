a573c9190b496f73a28c93efa1381589b3cdb645
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-17 23:31:20 +0400

    .

--- HomeWorks/HomeWork3/HomeWork3/Program.cs
+++ HomeWorks/HomeWork3/HomeWork3/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Calculator");
+            Console.Write("Enter your fNumber: ");
+            int fNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your sNumber: ");
+            int sNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operators (+,-,*,/): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            if (op == '+' || op =='-' || op == '*' || op == '/')
+            {
+                switch (op)
+                {
+                    case '+':
+                        int result1 = fNum + sNum;
+                        Console.WriteLine($"{fNum} {op} {sNum} = {result1}");
+                        break;
+                }
+            }
         }
     }
 }
