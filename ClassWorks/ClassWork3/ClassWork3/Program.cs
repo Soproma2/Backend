10d17d206356c130a65cf37754adbfec1e6a9240
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-18 21:17:34 +0400

    ClassWork3

---
+++ ClassWorks/ClassWork3/ClassWork3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ClassWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i=1; i < 11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+        }
+    }
+}
