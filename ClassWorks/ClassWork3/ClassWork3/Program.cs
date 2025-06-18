6f641fc9ea3d5101ab6f3f305fd6559170d4f7cb
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-18 21:23:24 +0400

    ClassWork3

--- ClassWorks/ClassWork3/ClassWork3/Program.cs
+++ ClassWorks/ClassWork3/ClassWork3/Program.cs
@@ -5,12 +5,27 @@ namespace ClassWork3
     {
         static void Main(string[] args)
         {
+            //დავალება 1
             //for(int i=1; i < 11; i++)
             //{
             //    Console.WriteLine(i);
             //}
 
 
+            //დავალება 2.1
+            //for(int i = 1; i <=20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //დავალება 2.2
+            //for(int i=2; i<=20; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
         }
     }
 }
