c4af4fc2a0e4e16da04bbed37c3cd04c86034952
Soproma2 <nikolozsopromadze@gmail.com>
2025-06-29 00:59:27 +0400

    HomeWork7

---
+++ HomeWorks/HomeWork7/HomeWork7/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HomeWork7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            numbers(0);
+            numbers(1);
+            numbers(2);
+            numbers(3);
+            Console.WriteLine();
+            getNames(0);
+            getNames(1);
+            getNames(2);
+        }
+
+
+        static void numbers(int i)
+        {
+            int[] ar = { 11, 12, 33, 44 };
+            Console.WriteLine(ar[i]);
+        }
+
+        static void getNames(int i)
+        {
+            Console.WriteLine("Enter Name(1): ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Enter Name(2): ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Enter Name(3): ");
+            string name3 = Console.ReadLine();
+            string[] names = { name1, name2, name3 };
+            Console.WriteLine(names[i]);
+        }
+    }
+}
