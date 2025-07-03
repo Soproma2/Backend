727bf3e5de92dd1283c0a73f88dc2ce98fcfbbde
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 17:41:00 +0400

    Add HomeWork6 project with array manipulation tasks
    This commit introduces a new project named "HomeWork6" to the solution, including its configuration in `HomeWork.sln` and a new `HomeWork6.csproj` file with .NET SDK settings.
    
    Additionally, a `Program.cs` file is added, featuring a `Main` method that implements various tasks on an integer array, such as summing elements, calculating length without using the `Length` property, printing the array in reverse, copying elements to another array, and identifying unique elements.

---
+++ Backend/HomeWork/HomeWork6/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace HomeWork6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, };
+            //დავალება 1 (დაწერეთ კოდი რომელიც შეაჯამებს მასივის ყველა ელემენტს)
+            int sum = default;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine();
+
+
+            //დავალება 2 (დაწერეთ კოდი რომელიც მოძებმის მასივის ზომას length - ის გამოყენების გარეშე)
+            int length = 0;
+            foreach (int i in ar)
+            {
+                length++;
+            }
+            Console.WriteLine(length);
+            Console.WriteLine();
+
+
+            //დავალება 3 (დაწერეთ კოდი რომელიც დაბეჭდავს მასივს უკუღმა)
+            for (int i = ar.Length - 1; i >= 0; i--)
+            {
+                Console.Write(ar[i] + ";");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            //დავალება 4 (დაწერეთ კოდი რომელიც მასივის ყველა ელემენტს გადააკოპირებს მეორე მასივში)
+            int[] ar2 = new int[ar.Length];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar2[i] = ar[i];
+            }
+            foreach (int num in ar2)
+            {
+                Console.Write(num + ";");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //დავალება 5 (დაწერეთ კოდი რომელიც მოძებნის მასივის უნიკალურ ელემენტებს)
+            for (int i = 0; i < ar.Length; i++)
+            {
+                bool unique = true;
+                for (int j = 0; j < ar.Length; j++)
+                {
+                    if (i != j && ar[i] == ar[j])
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+
+                if (unique)
+                {
+                    Console.Write(ar[i] + ";");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
