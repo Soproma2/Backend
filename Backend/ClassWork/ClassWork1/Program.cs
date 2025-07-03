166e8cbefda20ec451520fa3c57b73f99da8a94c
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 15:58:01 +0400

    Add ClassWork1 project with user input functionality
    This commit introduces a new project named "ClassWork1" to the solution, configured to use the .NET SDK targeting .NET 9.0. A project file `ClassWork1.csproj` has been created with necessary properties, and a new C# source file `Program.cs` includes a `Main` method that prompts the user for their first name, last name, and age, then displays a greeting message.

---
+++ Backend/ClassWork/ClassWork1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter your FirstName: ");
+                string FirstName = Console.ReadLine();
+
+                Console.Write("Enter your LastName: ");
+                string LastName = Console.ReadLine();
+
+                Console.Write("Enter your Age: ");
+                int Age = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.Write($"Hello my name is {FirstName} {LastName} and my age is {Age}");
+            }
+    }
+}
