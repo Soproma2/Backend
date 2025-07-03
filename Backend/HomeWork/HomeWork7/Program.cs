b132530c82c7ec23b100e4fecd42c7843bc563d3
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 17:44:28 +0400

    Add HomeWork7 project with array operations example
    - Introduced a new project "HomeWork7" in the solution.
    - Created `HomeWork7.csproj` targeting .NET 9.0 with SDK settings.
    - Added `Program.cs` with a `Main` method demonstrating array handling, user input, and basic operations like sorting and finding the minimum value.

---
+++ Backend/HomeWork/HomeWork7/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace HomeWork7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            numbers(0);
+            numbers(1);
+            numbers(2);
+            numbers(3);
+            Console.WriteLine();
+
+            Console.Write("Enter Name(1): ");
+            string name1 = Console.ReadLine();
+            Console.Write("Enter Name(2): ");
+            string name2 = Console.ReadLine();
+            Console.Write("Enter Name(3): ");
+            string name3 = Console.ReadLine();
+            string[] names = { name1, name2, name3 };
+            getNames(names, 0);
+            getNames(names, 1);
+            getNames(names, 2);
+            Console.WriteLine();
+
+            arrayMembers();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int[] array = [10, 5, 3, 4, 18,];
+            arraySort(array);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            arrayMin(array);
+        }
+
+
+        static void numbers(int i)
+        {
+            int[] ar = { 11, 12, 33, 44 };
+            Console.WriteLine(ar[i]);
+        }
+
+        static void getNames(string[] names, int i)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+        static void arrayMembers()
+        {
+            int[] ar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + ";");
+            }
+        }
+
+        static void arraySort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        int t = array[j];
+                        array[j] = array[i];
+                        array[i] = t;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ";");
+            }
+        }
+
+        static void arrayMin(int[] array)
+        {
+            int min = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
