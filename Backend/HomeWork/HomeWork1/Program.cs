ba53f2e16c1924ec049c32f1786603bff4869487
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 17:30:37 +0400

    Update Program.cs to collect user input for assignments
    Comment out the greeting and add prompts for user inputs related to films and personal information. This includes details for two assignments ("დავალება 1" and "დავალება 2") such as favorite films, directors, release years, and personal details like names, age, and marital status.

--- Backend/HomeWork/HomeWork1/Program.cs
+++ Backend/HomeWork/HomeWork1/Program.cs
@@ -4,7 +4,66 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // დავალება 1
+            //Console.Write("Enter your favorite film: ");
+            //string favoriteFilm = Console.ReadLine();
+            //Console.Write("Enter the first and last name of the film director: ");
+            //string filmDirector = Console.ReadLine();
+            //Console.Write("Enter the movie release year: ");
+            //int releaseYear = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Write a description of the movie: ");
+            //string description = Console.ReadLine();
+            //Console.Write("Enter the movie budget ($): ");
+            //int budget = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter IMDB rating: ");
+            //double imdbRating = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Is the film an Oscar winner?: ");
+            //string Oscar = Console.ReadLine();
+
+
+            //Console.WriteLine(" ");
+            //Console.WriteLine($"Your favorite film: {favoriteFilm}");
+            //Console.WriteLine($"Film director: {filmDirector}");
+            //Console.WriteLine($"Release year: {releaseYear}");
+            //Console.WriteLine($"Description: {description}");
+            //Console.WriteLine($"Movie budget: ${budget}");
+            //Console.WriteLine($"IMDB rating: {imdbRating}");
+            //Console.WriteLine($"Oscar winner: {Oscar}");
+
+
+
+
+
+
+            // დავალება 2
+            //Console.Write("Enter your First Name: ");
+            //string firstName = Console.ReadLine();
+            //Console.Write("Enter your Last Name: ");
+            //string lastName = Console.ReadLine();
+            //Console.Write("Enter your Personal number; ");
+            //int personalNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter your Age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter your Height (cm): ");
+            //double height = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter your Weight (kg): ");
+            //double weight = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Are you married? (yes/no): ");
+            //string married = Console.ReadLine();
+            //Console.Write("Do you have a pet? (yes/no): ");
+            //string pet = Console.ReadLine();
+
+
+
+            //Console.WriteLine(" ");
+            //Console.WriteLine($"First Name: {firstName}");
+            //Console.WriteLine($"Last Name: {lastName}");
+            //Console.WriteLine($"Personal Number: {personalNumber}");
+            //Console.WriteLine($"Age: {age}");
+            //Console.WriteLine($"Height: {height} cm");
+            //Console.WriteLine($"Weight: {weight} kg");
+            //Console.WriteLine($"Married: {married}");
+            //Console.WriteLine($"Pet: {pet}");
         }
     }
 }
