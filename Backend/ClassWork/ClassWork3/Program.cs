a3d14939a50099fabd909b78d68db2fa7450881a
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 15:24:30 +0400

    Update solution and add ClassWork3 project
    - Bump Visual Studio version in `ClassWork.sln` to 17.14.36221.1.
    - Add new project `ClassWork3` with .NET 9.0 settings.
    - Change `AssemblyInformationalVersion` in `ClassWork1.AssemblyInfo.cs`.
    - Update hash in `ClassWork1.AssemblyInfoInputs.cache`.
    - Introduce `Program.cs` for `ClassWork3` with commented code snippets.

---
+++ Backend/ClassWork/ClassWork3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ClassWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //დავალება 1
+            //for(int i=1; i < 11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //დავალება 2.1
+            //for(int i = 1; i <=20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //დავალება 2.2
+            //for(int i=2; i<=20; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int sum = 0;
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+
+
+            //Console.WriteLine("Enter your number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int factorial = 1;
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //   factorial *= i;
+            //}
+            //Console.WriteLine(factorial);
+
+            //for(int i=1; i<10; i++)
+            //{
+            //    for (int j=1; j<10; j++)
+            //    {
+            //        Console.WriteLine($"{i}*{j}={i * j}");
+            //    }
+            //}
+        }
+    }
+}
