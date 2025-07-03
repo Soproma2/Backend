9aaa7e363c4dd1330347f481db9c9e406de92609
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 15:34:12 +0400

    Add ClassWork5 project and update ClassWork1 metadata
    - Introduced a new project "ClassWork5" targeting .NET 9.0 with appropriate settings in `ClassWork.sln` and `ClassWork5.csproj`.
    - Updated `AssemblyInformationalVersion` in `AssemblyInfo.cs` for "ClassWork1".
    - Modified hash value in `AssemblyInfoInputs.cache`.
    - Created `Program.cs` for "ClassWork5" with methods for palindrome checks and other utilities, including a `Main` method with commented-out test code.

--- Backend/ClassWork/ClassWork1/obj/Debug/net9.0/ClassWork1.AssemblyInfo.cs
+++ Backend/ClassWork/ClassWork1/obj/Debug/net9.0/ClassWork1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassWork1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d14939a50099fabd909b78d68db2fa7450881a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485bac09ccaa46370553685e800668e5e2748929")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassWork1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassWork1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Backend/ClassWork/ClassWork5/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ClassWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(helloWorld());
+
+            //string userText = Console.ReadLine();
+            //Console.WriteLine(isTextPalindrom(userText));
+
+            //int[] ar = { 1, 2, 1 };
+            //Console.WriteLine(isArrayPalindrom(ar));
+
+
+            //string userText2 = Console.ReadLine();
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //print(userText2, num);
+
+            //int[] array = { 1, 2, 123, 12, 12, 1, 34, 223 };
+            //Console.WriteLine(FInd(array, 12));
+
+        }
+
+        static string helloWorld()
+        {
+            return "Hello World";
+        }
+
+        static string isTextPalindrom(string palindrom)
+        {
+            char[] userTextC = palindrom.ToCharArray();
+            Array.Reverse(userTextC);
+            string revStr = new string(userTextC);
+            return palindrom == revStr ? "The string is a palindrome." : "No, it's not a palindrome.";
+        }
+
+        static bool isArrayPalindrom(int[] array)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+
+            while (start < end)
+            {
+                if (array[start] != array[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+
+            return true;
+
+        }
+
+        static void print(string ar, int num)
+        {
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(ar);
+            }
+        }
+
+        static int FInd(int[] arr, int num)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    return arr[i];
+                }
+
+            }
+            return 0;
+        }
+    }
+}
