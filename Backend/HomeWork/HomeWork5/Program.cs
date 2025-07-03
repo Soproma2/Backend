c65807320328c352677b3ab3b25e6c4740ee3e49
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 17:39:35 +0400

    Add HomeWork5 project with console calculator
    This commit introduces a new project named "HomeWork5" to the solution. The project is configured for Debug and Release modes and is set up as an executable targeting .NET 9.0. It includes a `Program.cs` file that implements a console application for basic arithmetic operations, handling user input and exceptions for invalid entries and division by zero.

---
+++ Backend/HomeWork/HomeWork5/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace HomeWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    while (true)
+                    {
+                        int fNum;
+                        Console.Write("Enter your first number: ");
+                        while (!int.TryParse(Console.ReadLine(), out fNum))
+                        {
+                            throw new Exception("Invalid input! Please enter a valid integer");
+                        }
+
+                        int sNum;
+                        Console.Write("Enter your second number: ");
+                        while (!int.TryParse(Console.ReadLine(), out sNum))
+                        {
+                            throw new Exception("Invalid input! Please enter a valid integer");
+                        }
+
+                        char op;
+                        Console.Write("Enter operator (+, -, *, /): ");
+                        while (true)
+                        {
+                            string input = Console.ReadLine();
+
+                            if (input.Length == 1 && "+-*/".Contains(input))
+                            {
+                                op = input[0];
+                                break;
+                            }
+                            else
+                            {
+                                throw new Exception("Operator is incorrect! Please enter one of (+, -, *, /)");
+                            }
+                        }
+
+
+
+                        if (op == '+' || op == '-' || op == '*' || op == '/')
+                        {
+                            switch (op)
+                            {
+                                case '+':
+                                    int result1 = fNum + sNum;
+                                    Console.WriteLine($"{fNum} {op} {sNum} = {result1}");
+                                    break;
+                                case '-':
+                                    int result2 = fNum - sNum;
+                                    Console.WriteLine($"{fNum} {op} {sNum} = {result2}");
+                                    break;
+                                case '*':
+                                    int result3 = fNum * sNum;
+                                    Console.WriteLine($"{fNum} {op} {sNum} = {result3}");
+                                    break;
+                                case '/':
+                                    if (sNum == 0)
+                                    {
+                                        throw new DivideByZeroException("Cannot divide by zero!");
+                                    }
+                                    int result4 = fNum / sNum;
+                                    Console.WriteLine($"{fNum} {op} {sNum} = {result4}");
+                                    break;
+                                default:
+                                    throw new Exception("Unknown operator!");
+                            }
+                        }
+
+                        Console.Write("Close application (x=yes or other character=no): ");
+                        char exitInput = Convert.ToChar(Console.ReadLine());
+                        if (exitInput == 'x' || exitInput == 'X')
+                        {
+                            Console.WriteLine("Application closed.");
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"Error: {ex.Message}");
+                    Console.WriteLine("Let's try again.");
+                    Console.WriteLine();
+                    continue;
+                }
+            }
+        }
+    }
+}
