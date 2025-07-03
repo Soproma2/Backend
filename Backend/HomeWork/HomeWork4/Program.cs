c5461964ec7780483095f294f6153e1392a63707
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-03 17:37:52 +0400

    Add HomeWork4 project with initial implementation
    - Created a new project "HomeWork4" in the solution.
    - Added `HomeWork4.csproj` with .NET SDK targeting net9.0.
    - Introduced `Program.cs` with a `Main` method and commented tasks for user input and output.

---
+++ Backend/HomeWork/HomeWork4/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //დავალება  1 (მომხმარებელს კლავიატურის გამოყენებით შეჰყავს 2 რიცხვი.ჩვენი ამოცანაა, რომ ვაჩვენოთ ყველა ლუწი რიცხვი მითითებულ დიაპაზონში.თუ დიაპზონის საზღვრები არასწორად არის მოცემული, თქვენ უნდა ჩაასწოროთ ის.მაგალითად, თუ მომხმარებელმა შეიყვანა 20 და 11, ჩასწორებაა საჭირო, რადგან დიაპაზონის დასაწყისი უნდა იყოს - 11, და დასასრული - 20.)
+
+            //Console.WriteLine("Find even numbers from x to y.");
+            //Console.Write("Enter x: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter y: ");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("");
+
+            //if (x < y)
+            //{
+            //    for (int i=x;  i<=y; i++)
+            //    {
+            //        if (i % 2 == 0)
+            //        {
+            //            Console.Write($"{i};");
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The numbers you entered are invalid.");
+            //    Console.WriteLine($"You probably mean from {y} to {x}.");
+            //    Console.WriteLine("");
+            //    for (int i = y; i <= x; i++)
+            //    {
+            //        if (i % 2 == 0)
+            //        {
+            //            Console.Write($"{i};");
+            //        }
+            //    }
+            //}
+
+
+
+            //დავალება 2 (დაწერეთ პროგრამა რომელიც გამოიტანს კონსოლში ფიფქებით შედგენილ ნახევარპირამიდის ფორმას.)
+
+            //Console.WriteLine("Enter number: ");
+            //int uNum = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= uNum; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //დავალება 3 (დაწერეთ პროგრამა რომელიც კონსოლიდან წაკითხული რიცხვის მიხედვით გამოიტანს ციფრებით გამოსახულ პირამიდას.)
+
+            //Console.WriteLine("Enter number: ");
+            //int uNum = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= uNum; i++)
+            //{
+            //    for (int k = 1; k <= uNum - i; k++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(i+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
