b3d6721456228add8682fbc2dfe164d51c066414
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-10 18:11:37 +0400

    Add HomeWork11 project and character classes
    - Updated solution file to include "HomeWork11" project.
    - Modified `HomeWork2.csproj` to reference "HomeWork10".
    - Created `HomeWork11.csproj` with necessary project properties.
    - Added `Program.cs` with a basic structure for the main class.
    - Introduced `Character.cs` with an abstract class and derived classes `Hero` and `Villain`.

---
+++ Backend/HomeWork/HomeWork11/Class/Character.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11.Class
+{
+
+     
+    //📌 ტექნიკური მოთხოვნები:
+    //1. აბსტრაქცია
+    //შექმენი აბსტრაქტული კლასი Character, რომელიც წარმოადგენს ზოგად პერსონაჟს.
+    //უნდა შეიცავდეს შემდეგ თვისებებს:
+    //string Name
+    //int Health
+    //უნდა ჰქონდეს შემდეგი მეთოდები:
+    //abstract void Attack(Character target) – შეტევის აბსტრაქტული მეთოდი.
+    //void TakeDamage(int damage) – რომელიც ამცირებს პერსონაჟის სიცოცხლეს.
+    //bool IsAlive – რომელიც ამოწმებს პერსონაჟი ცოცხალია თუ არა.
+
+    public abstract class Character
+    {
+        public string Name { get; set; }
+        private int health;
+
+        public int Health
+        {
+            get { return health; }
+            private set { health = 100;}
+        }
+
+        public string Weapon { get; set; }
+        public abstract void Attack(Character target);
+
+        public void takeDamage(int damage)
+        {
+            if (damage > 0)
+            {
+                Health -= damage;
+                if (Health < 0)
+                {
+                    Health = 0;
+                }
+            }
+        }
+
+        public bool IsAlive
+        {
+            get {  return Health > 0;}
+        }
+    }
+
+
+
+    //2. მემკვიდრეობა
+    //შექმენი ორი კლასი Hero და Villain, რომლებიც მემკვიდრეობას იღებენ Character-ისგან.
+    //თითოეულმა კლასმა უნდა დააიმპლემენტოს საკუთარი ვერსია Attack მეთოდისა.მაგალითად:
+    //Hero იყენებს ხმალს (sword) და აზიანებს მოწინააღმდეგეს 20 ქულით.
+    //Villain იყენებს ჯადოს (fireball) და აზიანებს მოწინააღმდეგეს 25 ქულით.
+
+
+    public class Hero : Character
+    {
+        
+        public override void Attack(Character target)
+        {
+            
+        }
+    }
+
+    public class Villain : Character
+    {
+        public override void Attack(Character target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
