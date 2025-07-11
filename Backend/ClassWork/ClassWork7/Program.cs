b5a5739f115e95d4c074d850dbb822e4b1936aaf
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-11 21:59:21 +0400

    Add ClassWork7 project with age calculation functionality
    - Introduced a new project "ClassWork7" in the solution.
    - Created `ClassWork7.csproj` targeting .NET 9.0 with necessary configurations.
    - Added `Program.cs` containing a method to calculate age from birth date.

---
+++ Backend/ClassWork/ClassWork7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ClassWork7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //დაწერეთ ფუნქცია რომელსაც გადავცემთ, დაბადების თარიღს და ფუნქცია დაგვიბრუნებს თუ რამდენი წლისაა ადამიანი 
+
+            Console.WriteLine(age(new DateTime(2008, 02, 27)));
+        }
+
+        public static int age(DateTime birthtime)
+        {
+            DateTime dateNow = DateTime.Now;
+            var age = dateNow.Year - birthtime.Year;
+
+
+            if (dateNow < birthtime.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
