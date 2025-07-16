6ada114be6012cc568305f68576cbe1492d04272
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-16 21:50:31 +0400

    .

---
+++ Backend/ClassWork/ClassWork8/Class/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork8.Class
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Cylinder { get; set; }
+        public decimal Engine { get; set; }
+        public string Drive { get; set; }
+        public string Transmission { get; set; }
+        public int City { get; set; }
+        public int Combined { get; set; }
+        public int Highway { get; set; }
+
+        public Car(string Make, string Model, int Cylinder, decimal Engine,string Drive,
+        string Transmission, int City, int Combined, int Highway)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Cylinder = Cylinder;
+            this.Engine = Engine;
+            this.Drive = Drive;
+            this.Transmission = Transmission;
+            this.City = City;
+            this.Combined = Combined;
+            this.Highway = Highway;
+        }
+
+    }
+}
---
+++ Backend/ClassWork/ClassWork8/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassWork8.Class;
+
+namespace ClassWork8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] result = File.ReadAllLines(@"../../../vehicles.csv");
+
+            
+
+            
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                string[] sT = result[i].Split(',');
+                Car car1 = new Car(sT[0], sT[1], Convert.ToInt32(sT[2]), Convert.ToDecimal(sT[3]), sT[4], sT[5], Convert.ToInt32(sT[6]), Convert.ToInt32(sT[7]), Convert.ToInt32(sT[8]));
+                Console.WriteLine($"{car1.Make}; {car1.Model}; {car1.Cylinder}; {car1.Engine}; {car1.Drive}; {car1.Transmission}; {car1.City}; {car1.Combined}; {car1.Highway}");
+            }
+        }
+    }
+}
