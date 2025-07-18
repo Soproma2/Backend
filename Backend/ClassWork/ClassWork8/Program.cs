e88e66c0de5c3756c9322a5318330b77c3d95b6f
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-18 16:12:50 +0400

    .

--- Backend/ClassWork/ClassWork8/Program.cs
+++ Backend/ClassWork/ClassWork8/Program.cs
@@ -21,50 +21,27 @@ namespace ClassWork8
             {
                 sT = result[i].Split(',');
                 car = new Car(sT[0], sT[1], Convert.ToInt32(sT[2]), Convert.ToDecimal(sT[3]), sT[4], sT[5], Convert.ToInt32(sT[6]), Convert.ToInt32(sT[7]), Convert.ToInt32(sT[8]));
-                // Console.WriteLine($"{car.Make}; {car.Model}; {car.Cylinder}; {car.Engine}; {car.Drive}; {car.Transmission}; {car.City}; {car.Combined}; {car.Highway}");
+
+                
                 if (car.Make == "Mercedes-Benz")
                 {
                     Console.WriteLine($"{car.Make}; {car.Model}; {car.Cylinder}; {car.Engine}; {car.Drive}; {car.Transmission}; {car.City}; {car.Combined}; {car.Highway}");
                 }
+
                 cars.Add(car);
-
-
             }
+
             Console.WriteLine();
 
-            List<Car> top10 = new List<Car>();
+            
+            List<Car> top10 = cars
+                .GroupBy(c => new { c.Make, c.Model })
+                .Select(g => g.OrderByDescending(c => c.Combined).First())
+                .OrderByDescending(c => c.Combined)
+                .Take(10)
+                .ToList();
 
-            foreach (var c in cars)
-            {
-                if (top10.Count < 10)
-                {
-                    top10.Add(c);
-                }
-                else
-                {
-                    int minIndex = 0;
-                    int minCombined = top10[0].Combined;
-
-                    for (int i = 1; i < top10.Count; i++)
-                    {
-                        if (top10[i].Combined < minCombined)
-                        {
-                            minCombined = top10[i].Combined;
-                            minIndex = i;
-                        }
-                    }
-
-
-                    if (c.Combined > minCombined)
-                    {
-                        top10[minIndex] = c;
-                    }
-                }
-            }
-
-
-            top10.Sort((a, b) => b.Combined.CompareTo(a.Combined));
-
+            Console.WriteLine("Top 10 Most Fuel-Efficient Cars:");
             foreach (var c in top10)
             {
                 Console.WriteLine($"{c.Make} {c.Model} | Combined: {c.Combined} MPG");
