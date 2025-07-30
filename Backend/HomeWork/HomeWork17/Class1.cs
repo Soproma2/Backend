d90ac357c4428a8190ec5a198c979ec47208f828
Soproma2 <nikolozsopromadze@gmail.com>
2025-07-30 19:11:51 +0400

    .

---
+++ Backend/HomeWork/HomeWork17/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork17
+{
+    internal class Class1
+    {
+        public static T Custom_Max<T>(this List<T> intList) where T : IComparable<T>
+        {
+            T max = intList[0];
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                if (intList[i].CompareTo(max) > 0)
+                {
+                    max = intList[i];
+                }
+            }
+
+            return max;
+        }
+        public static T Custom_Min<T>(this List<T> intList) where T : IComparable<T>
+        {
+            T min = intList[0];
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                if (intList[i].CompareTo(min) < 0)
+                {
+                    min = intList[i];
+                }
+            }
+
+            return min;
+        }
+        public static List<T> Custom_Take<T>(this List<T> intList, int count)
+        {
+            List<T> result = new();
+
+            for (int i = 0; i < count && i < intList.Count; i++)
+            {
+                result.Add(intList[i]);
+            }
+
+            return result;
+        }
+        public static List<T> Custom_Skip<T>(this List<T> intList, int count)
+        {
+            List<T> result = new();
+
+            for (int i = count; i < intList.Count; i++)
+            {
+                result.Add(intList[i]);
+            }
+
+            return result;
+        }
+    }
+}
