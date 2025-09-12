9cc3713c9ff651f7d2e46896e46cad086385fdf2
Soproma2 <nikolozsopromadze@gmail.com>
2025-09-12 19:52:30 +0400

    .

--- Backend/HomeWork/HomeWork19/Program.cs
+++ Backend/HomeWork/HomeWork19/Program.cs
@@ -42,6 +42,37 @@
                 Console.Write("Choose an option: ");
 
                 string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        break;
+
+                    case "5":
+                        break;
+
+                    case "6":
+                        break;
+
+                    case "7":
+                        break;
+
+                    case "8":
+                        break;
+
+                    case "9":
+                        break;
+
+                }
             }
 
 
