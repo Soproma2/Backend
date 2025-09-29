d29c4c505a2c5c5781eeba4536c839a51ca90638
Soproma2 <nikolozsopromadze@gmail.com>
2025-09-29 22:07:36 +0400

    .

---
+++ Backend/ClassWork/ClassWork16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace ClassWork16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uservalidation validator = new Uservalidation();
+
+            Console.WriteLine("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+
+            User user = new User() {  Username = username, Email = email, Password = password };
+
+            var result = validator.Validate(user);
+            if (!result.IsValid)
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+                return;
+            }
+
+            Console.WriteLine("User registered!");
+
+        }
+    }
+}
---
+++ Backend/ClassWork/ClassWork16/Uservalidation.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork16
+{
+    public class Uservalidation : AbstractValidator<User>
+    {
+        public Uservalidation()
+        {
+            RuleFor(e=> e.Username)
+                .NotEmpty().WithMessage("tu shevseba ar gindoda ras mawuxebdi")
+                .MinimumLength(3).WithMessage("gtxov minimum 3 simbolo sheiyvane");
+            RuleFor(e=> e.Email)
+                .NotEmpty().WithMessage("tu shevseba ar gindoda ras mawuxebdi")
+                .EmailAddress().WithMessage("emailis truqturac me unda gaswavlo?");
+            RuleFor(e=> e.Password)
+                .NotEmpty().WithMessage("tu shevseba ar gindoda ras mawuxebdi")
+                .MinimumLength(8).WithMessage("gtxov minimum 8 simbolo sheiyvane");
+        }
+    }
+}
