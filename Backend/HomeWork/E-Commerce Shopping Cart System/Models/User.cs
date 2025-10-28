c3eb95153cbf31a73ebc7726102af4585a670173
Soproma2 <nikolozsopromadze@gmail.com>
2025-10-28 22:52:47 +0400

    .

--- Backend/HomeWork/E-Commerce Shopping Cart System/Models/User.cs
+++ Backend/HomeWork/E-Commerce Shopping Cart System/Models/User.cs
@@ -15,7 +15,6 @@ namespace E_Commerce_Shopping_Cart_System.Models
         public string Password { get; set; }
         public UserRole Role { get; set; } = UserRole.USER;
         public double Balance { get; set; } = 1000;
-        public bool IsActive { get; set; } = true;
 
         public List<CartItem> CartItems { get; set; } = new List<CartItem>();
         public List<Order> Orders{ get; set; } = new List<Order>();
