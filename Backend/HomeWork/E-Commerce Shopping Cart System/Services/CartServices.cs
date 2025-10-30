0589f4ec8d7e4201710824ceda61f4ad11937692
Soproma2 <nikolozsopromadze@gmail.com>
2025-10-30 11:11:00 +0400

    .

--- Backend/HomeWork/E-Commerce Shopping Cart System/Services/CartServices.cs
+++ Backend/HomeWork/E-Commerce Shopping Cart System/Services/CartServices.cs
@@ -38,17 +38,44 @@ namespace E_Commerce_Shopping_Cart_System.Services
             JsonHelper.SaveData(path, CartItems);
             Console.WriteLine("Added to cart!");
         }
-        public void RemoveFromCart(User user)
+        public static void RemoveFromCart(User user)
         {
-
+            Console.Write("Enter Product ID to remove: ");
+            if (!int.TryParse(Console.ReadLine(), out int pid)) { Console.WriteLine("Invalid ID!"); return; }
+            var cart = CartItems.FirstOrDefault(c => c.User.Id == user.Id && c.Product.Id == Convert.ToString(pid));
+            if (cart == null) { Console.WriteLine("Not in cart!"); return; }
+            CartItems.Remove(cart);
+            JsonHelper.SaveData(path, CartItems);
+            Console.WriteLine("Removed from cart!");
         }
-        public void ViewCart()
+        public static void ViewCart(User user)
         {
-
+            var cart = CartItems.Where(c => c.User.Id == user.Id).ToList();
+            if (cart.Count == 0) { Console.WriteLine("Cart empty!"); return; }
+            double total = 0;
+            foreach (var c in cart)
+            {
+                var p = ProductServices.Products.FirstOrDefault(x => x.Id == c.Id);
+                if(p != null)
+                {
+                    double price = c.Quantity * p.Price;
+                    total += price;
+                    Console.WriteLine($"{p.Name} x {c.Quantity} = {price}$");
+                }
+            }
+            Console.WriteLine($"Total: {total}$");
         }
-        public void UpdateCartQuantity()
+        public static void UpdateCartQuantity(User user)
         {
-
+            Console.Write("Enter Product Id to update: ");
+            if (!int.TryParse(Console.ReadLine(), out int pid)) { Console.WriteLine("Invalid ID!"); return; }
+            var cart = CartItems.FirstOrDefault(c => c.User.Id == user.Id && c.Product.Id == Convert.ToString(pid));
+            if (cart == null) { Console.WriteLine("Not in cart!"); return; }
+            Console.Write("Enter new quantity: ");
+            if (!int.TryParse(Console.ReadLine(), out int qty)) { Console.WriteLine("Invalid quantity!"); return; }
+            cart.Quantity = qty;
+            JsonHelper.SaveData(path, CartItems);
+            Console.WriteLine("Cart updated!");
         }
     }
 }
