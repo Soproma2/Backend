100d90be11819a93e58b2ac50f785577f2a1cf6b
Soproma2 <nikolozsopromadze@gmail.com>
2025-10-30 10:02:53 +0400

    .

--- Backend/HomeWork/E-Commerce Shopping Cart System/Services/CartServices.cs
+++ Backend/HomeWork/E-Commerce Shopping Cart System/Services/CartServices.cs
@@ -1,4 +1,5 @@
 ﻿using E_Commerce_Shopping_Cart_System.Models;
+using E_Commerce_Shopping_Cart_System.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,66 +9,36 @@ using System.Threading.Tasks;
 
 namespace E_Commerce_Shopping_Cart_System.Services
 {
-    internal class CartServices
+    public static class CartServices
     {
-        static string cartFile = "cart.json";
-        static User currentUser = new User();
-        static List<Product> products = new List<Product>();
-        public void AddToCart()
+        public static string path = "Data/Cart.json";
+        public static List<CartItem> CartItems = JsonHelper.LoadData<CartItem>(path);
+        public static void AddToCart(User user)
         {
-            Console.WriteLine("Available Products: ");
-            foreach (var item in products)
-            {
-                Console.WriteLine($"{item.Id}. {item.Name} - {item.Price} $");
-            }
+            Console.Write("Enter Product Id to add: ");
+            if (!int.TryParse(Console.ReadLine(), out int pid)) { Console.WriteLine("Invalid Id!"); return; }
+            var product = ProductServices.Products.FirstOrDefault(p => p.Id == Convert.ToString(pid));
+            if (product == null) { Console.WriteLine("Product not found!"); return; }
 
-            Console.Write("Select product number to add to cart: ");
-            if (!int.TryParse(Console.ReadLine(), out int productIndex) || productIndex < 1 || productIndex > products.Count)
-            {
-                Console.WriteLine("Invalid selection!");
-                return;
-            }
+            Console.Write("Enter quantity: ");
+            if(!int.TryParse(Console.ReadLine(), out int qty)) { Console.WriteLine("Invalid quantity!"); return; }
+            if (qty > product.Stock) { Console.WriteLine("Not enough stock!"); return; }
 
-            Product selectedProduct = products[productIndex - 1];
-
-            Console.Write($"Enter quantity for {selectedProduct.Name}: ");
-            if (!int.TryParse(Console.ReadLine(), out int quantity) || quantity <= 0)
-            {
-                Console.WriteLine("Invalid quantity!");
-                return;
-            }
-
-            List<CartItem> cartItems = new List<CartItem>();
-            if (File.Exists(cartFile))
-            {
-                string existingJson = File.ReadAllText(cartFile);
-                cartItems = JsonSerializer.Deserialize<List<CartItem>>(existingJson);
-            }
-
-            CartItem existingItem = cartItems.Find(ci => ci.User.Username == currentUser.Username && ci.Product.Name == selectedProduct.Name);
-
-            if (existingItem != null)
-            {
-                existingItem.Quantity += quantity;
-            }
+            var cart = CartItems.FirstOrDefault(c => c.User.Id == user.Id && c.Product.Id == Convert.ToString(pid));
+            if (cart != null) cart.Quantity += qty;
             else
             {
-                cartItems.Add(new CartItem
+                CartItems.Add(new CartItem
                 {
-                    User = currentUser,
-                    Product = selectedProduct,
-                    Quantity = quantity
+                    User = user,
+                    Product = product,
+                    Quantity = qty,
                 });
-
             }
-
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            string updatedJson = JsonSerializer.Serialize(cartItems, options);
-            File.WriteAllText(cartFile, updatedJson);
-
-            Console.WriteLine($"Added {quantity} x {selectedProduct.Name} to cart!");
+            JsonHelper.SaveData(path, CartItems);
+            Console.WriteLine("Added to cart!");
         }
-        public void RemoveFromCart()
+        public void RemoveFromCart(User user)
         {
 
         }
