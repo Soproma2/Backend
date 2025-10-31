5de8833834b0d2143d8e6d017111f21da97d6c7e
Soproma2 <nikolozsopromadze@gmail.com>
2025-10-31 19:20:04 +0400

    .

--- Backend/HomeWork/E-Commerce Shopping Cart System/Services/AuthSevices.cs
+++ Backend/HomeWork/E-Commerce Shopping Cart System/Services/AuthSevices.cs
@@ -13,26 +13,50 @@ namespace E_Commerce_Shopping_Cart_System.Services
     {
         public static string path = "Data/Users.json";
         public static List<User> Users = JsonHelper.LoadData<User>(path);
+        
         public static void Register()
         {
             Console.Clear();
             Console.WriteLine("\n----- Register New User -----");
+
             Console.Write("Enter username: ");
             string username = Console.ReadLine();
+
+            
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                Console.WriteLine("Username cannot be empty!");
+                return;
+            }
 
             Console.Write("Enter email: ");
             string email = Console.ReadLine();
 
+            
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("@") || !email.Contains("."))
+            {
+                Console.WriteLine("Invalid email format!");
+                return;
+            }
+
             Console.Write("Enter password: ");
             string password = Console.ReadLine();
 
-            if(Users.Any(u => u.Username == username || u.Email == email))
+            
+            if (string.IsNullOrWhiteSpace(password) || password.Length < 6)
             {
-                Console.WriteLine("User already exists!");
+                Console.WriteLine("Password must be at least 6 characters long!");
+                return;
+            }
+
+            if (Users.Any(u => u.Username == username || u.Email == email))
+            {
+                Console.WriteLine("User with this username or email already exists!");
                 return;
             }
 
             int id = Users.Count > 0 ? Users.Max(u => u.UserId) + 1 : 1;
+
             Users.Add(new User
             {
                 UserId = id,
@@ -45,6 +69,7 @@ namespace E_Commerce_Shopping_Cart_System.Services
             JsonHelper.SaveData(path, Users);
             Console.WriteLine("Registration successful!");
         }
+
 
         public static User Login()
         {
