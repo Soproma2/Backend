218ccc080b0bd26de1b9fcaedabf1fbd5ae4c882
Soproma2 <nikolozsopromadze@gmail.com>
2025-11-22 18:40:55 +0400

    .

---
+++ Backend/HomeWork/HomeWork36/Services/SMTPService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork36.Services
+{
+    internal class SMTPService
+    {
+        private string _email = "stepnika4@gmail.com";
+        private string _password = "jawx suta olou blih";
+
+        public void SendEmail(string subject, string email, string body)
+        {
+            var mail = new MailMessage();
+
+            mail.From = new MailAddress(_email, "Soproma");
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.To.Add(email);
+            mail.IsBodyHtml = false;
+
+            var smtp = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(_email, _password)
+            };
+
+            smtp.Send(mail);
+        }
+    }
+}
