08efa9997701919cfbab3476a7398e91987387d5
Soproma2 <nikolozsopromadze@gmail.com>
2025-12-13 23:51:11 +0400

    .

---
+++ Backend/HomeWork/HomeWork42 - Asp/Controllers/QuestionsController.cs
@@ -0,0 +1,44 @@
+﻿using HomeWork42___Asp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork42___Asp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly DataContext _db;
+        public QuestionsController(DataContext db) { _db = db; }
+
+        [HttpGet]
+        public IActionResult getAllQuestion()
+        {
+            return Ok();
+        }
+
+        [HttpGet("question/{questionId}")]
+        public IActionResult getQuestionById(int questionId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateQuestion()
+        {
+            return Ok();
+        }
+
+        [HttpPut("question/edit/{questionId}")]
+        public IActionResult UpdateQuestion(int questionId)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("question/delete/{questionId}")]
+        public IActionResult DeleteQuestion(int questionId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Backend/HomeWork/HomeWork42 - Asp/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using HomeWork42___Asp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeWork42___Asp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
