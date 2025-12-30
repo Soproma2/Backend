f4439154ca33c3ef5b1afec2d68cbfa2dc23bf02
Soproma2 <nikolozsopromadze@gmail.com>
2025-12-30 15:28:59 +0400

    .

---
+++ Backend/HomeWork/Homework 47 - Asp/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Homework_47___Asp.DTOs.Requests;
+using Homework_47___Asp.Services.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_47___Asp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _us;
+        public UsersController(IUserService us)
+        {
+            _us = us;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _us.GetUsers();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser(UserCreateReq req)
+        {
+            var user = _us.RegisterUser(req);
+            return Ok(user);
+        }
+    }
+}
--- Backend/HomeWork/Homework 47 - Asp/Services/User/IUserService.cs
+++ Backend/HomeWork/Homework 47 - Asp/Services/User/IUserService.cs
@@ -5,6 +5,6 @@ namespace Homework_47___Asp.Services.User
     public interface IUserService
     {
         List<UserResponse>? GetUsers();
-        UserResponse RegisterUser();
+        UserResponse RegisterUser(UserCreateReq req);
     }
 }
