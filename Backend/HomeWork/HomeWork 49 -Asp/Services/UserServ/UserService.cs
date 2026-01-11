bfa6fc8d63d5211e8d3651e8967cf0f6e1818c26
Soproma2 <nikolozsopromadze@gmail.com>
2026-01-11 13:04:54 +0400

    .

--- Backend/HomeWork/HomeWork 49 -Asp/Services/UserServ/UserService.cs
+++ Backend/HomeWork/HomeWork 49 -Asp/Services/UserServ/UserService.cs
@@ -12,22 +12,105 @@ namespace HomeWork_49__Asp.Services.UserServ
 
         public ApiResponse<string> ChangePassword(int userId, ChangePasswordRequest req)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.Find(userId);
+            if (user == null)
+            {
+                return new ApiResponse<string>()
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Message = "User not found"
+                };
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(req.CurrentPassword, user.Password))
+            {
+                return new ApiResponse<string>()
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Message = "Incorrect password"
+                };
+            }
+
+            user.Password = BCrypt.Net.BCrypt.HashPassword(req.NewPassword);
+            _db.SaveChanges();
+
+            return new ApiResponse<string>()
+            {
+                Status = StatusCodes.Status200OK,
+                Message = "Password changed successfully"
+            };
         }
 
         public ApiResponse<string> DeleteUser(int userId)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.Find(userId);
+            if (user == null)
+            {
+                return new ApiResponse<string>()
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Message = "User not found"
+                };
+            }
+
+            _db.Users.Remove(user);
+            _db.SaveChanges();
+
+            return new ApiResponse<string>()
+            {
+                Status= StatusCodes.Status200OK,
+                Message = "User deleted successfully"
+            };
+
         }
 
         public ApiResponse<string> EditUser(int userId, EditUserRequest req)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.Find(userId);
+            if (user == null)
+            {
+                return new ApiResponse<string>()
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Message = "User not found"
+                };
+            }
+
+            if (!string.IsNullOrWhiteSpace(req.Name))
+            {
+                user.Name = req.Name;
+            }
+
+            _db.SaveChanges();
+            return new ApiResponse<string>()
+            {
+                Status = StatusCodes.Status200OK,
+                Message = "User updated successfully"
+            };
         }
 
         public ApiResponse<UserResponse> GetUserById(int userId)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.Find(userId);
+            if (user == null)
+            {
+                return new ApiResponse<UserResponse>()
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Message = "User not found"
+                };
+            }
+
+            return new ApiResponse<UserResponse>()
+            {
+                Status = StatusCodes.Status200OK,
+                Value = new UserResponse()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email
+                }
+            };
         }
     }
 }
