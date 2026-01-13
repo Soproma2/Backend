33a9c4f711cf0c51e5c21fa64f764df20ee3d0dd
Soproma2 <nikolozsopromadze@gmail.com>
2026-01-13 15:01:52 +0400

    .

--- Backend/HomeWork/Entity_Project/Data/DataContext.cs
+++ Backend/HomeWork/Entity_Project/Data/DataContext.cs
@@ -11,9 +11,10 @@ namespace Entity_Project.Data
     internal class DataContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
