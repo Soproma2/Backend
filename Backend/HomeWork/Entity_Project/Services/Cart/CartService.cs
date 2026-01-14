2a884c4b3728d813ccc54426c5af5b1e29de18be
Soproma2 <nikolozsopromadze@gmail.com>
2026-01-14 17:08:12 +0400

    .

--- Backend/HomeWork/Entity_Project/Services/Cart/CartService.cs
+++ Backend/HomeWork/Entity_Project/Services/Cart/CartService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Entity_Project.Data;
+using Entity_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,90 @@ namespace Entity_Project.Services.Cart
 {
     internal class CartService : ICartService
     {
+        private readonly DataContext _db = new DataContext();
+        public void AddToCart(Models.User user)
+        {
+            if (user == null) throw new Exception("User not logged in!");
+
+            Console.Write("Enter Product Id to Add: ");
+            if (!int.TryParse(Console.ReadLine(), out int productId))
+                throw new Exception("Invalid Product ID!");
+
+            var product = _db.Products.Find(productId);
+            if (product == null) throw new Exception("Product not found!");
+
+            Console.Write($"Enter quantity (Available: {product.Stock})");
+            if (!int.TryParse(Console.ReadLine(), out int quantity) || quantity <= 0)
+                throw new Exception("Invalid quantity!");
+
+            if (quantity > product.Stock)
+                throw new Exception("not eanogh stock available!");
+
+            var existingCarItem = _db.CartItems
+                .FirstOrDefault(c=>c.UserId == user.Id && c.ProductId == productId);
+
+            if(existingCarItem != null)
+            {
+                if (existingCarItem.Quantity + quantity > product.Stock)
+                    throw new Exception("Adding this quantity exceeds available stock!");
+
+                existingCarItem.Quantity += quantity;
+            }
+            else
+            {
+                CartItem cartItem = new CartItem()
+                {
+                    UserId = user.Id,
+                    ProductId = productId,
+                    Quantity = quantity
+                };
+                _db.CartItems.Add(cartItem);
+            }
+
+            _db.SaveChanges();
+            Console.WriteLine("Added to cart successfully");
+        }
+        public void ViewCart(Models.User user)
+        {
+            var cartItems = _db.CartItems
+                .Include(c => c.Product)
+                .Where(c => c.UserId == user.Id)
+                .ToList();
+
+            if (!cartItems.Any())
+            {
+                Console.WriteLine("/nYour cart is empty.");
+                return;
+            }
+
+            Console.WriteLine("/n=== Your Shopping Cart ===");
+            double grandTotal = 0;
+
+            foreach (var item in cartItems)
+            {
+                double total = item.Product.Price * item.Quantity;
+                grandTotal += total;
+                Console.WriteLine($"ID: {item.ProductId} | {item.Product.Name} | Qty: {item.Quantity} | Price: {item.Product.Price}$ | Total: {total}$");
+            }
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine($"Grand Total: {grandTotal}$");
+        }
+
+        public void ClearCart(Models.User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromCart(Models.User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCartQuantity(Models.User user)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
