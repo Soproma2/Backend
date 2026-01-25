5f130b1b9bfbce35746746a29da0f47d1c2dc291
Soproma2 <nikolozsopromadze@gmail.com>
2026-01-25 12:02:31 +0400

    .

--- Backend/HomeWork/HomeWork 52 - Asp&angular (movies)/Controllers/MoviesController.cs
+++ Backend/HomeWork/HomeWork 52 - Asp&angular (movies)/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HomeWork_52___Asp_angular__movies_.DTOs.Request;
+using HomeWork_52___Asp_angular__movies_.Services.Movies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeWork_52___Asp_angular__movies_.Controllers
@@ -7,5 +9,42 @@ namespace HomeWork_52___Asp_angular__movies_.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+        private readonly IMoviesService _moviesService;
+        public MoviesController(IMoviesService moviesService) => _moviesService = moviesService;
+       
+        [HttpPost]
+        public IActionResult CreateMovie(CreateMovieDto req)
+        {
+            var result = _moviesService.CreateMovie(req);
+            return StatusCode(result.Status, result);
+        }
+
+        [HttpPut("{movieId}")]
+        public IActionResult UpdateMovie(UpdateMovieDto req, int movieId)
+        {
+            var result = _moviesService.UpdateMovie(req, movieId);
+            return StatusCode(result.Status, result);
+        }
+
+        [HttpDelete("{movieId}")]
+        public IActionResult DeleteMovie(int movieId)
+        {
+            var result = _moviesService.DeleteMovie(movieId);
+            return StatusCode(result.Status, result);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            var result = _moviesService.GetAllMovies();
+            return StatusCode(result.Status, result);
+        }
+
+        [HttpGet("{movieId}")]
+        public IActionResult GetMovieById(int movieId)
+        {
+            var result = _moviesService.GetMoviesById(movieId);
+            return StatusCode(result.Status, result);
+        }
     }
 }
